5ef2705eae86456a2a392bce3eae6dad13390e1d
Gnadi <johannes.gnadlinger1997@gmail.com>
2017-07-28 17:41:04 +0200

    added horizontal viewer

--- app/src/main/java/at/jo/gnadl/alfred/MainActivity.kt
+++ app/src/main/java/at/jo/gnadl/alfred/MainActivity.kt
@@ -1,6 +1,8 @@
 package at.jo.gnadl.alfred
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.support.annotation.IdRes
 import android.support.design.widget.Snackbar
@@ -18,22 +20,78 @@ import at.jo.gnadl.alfred.MainActivity.MyPagerAdapter
 import android.widget.Toast
 import android.support.v4.view.ViewPager.OnPageChangeListener
 import kotlinx.android.synthetic.main.content_main.*
+import android.os.CountDownTimer
+import android.util.Log
+import android.util.TypedValue
+import android.widget.TextView
+import android.widget.LinearLayout
+import android.view.LayoutInflater
+
+
+
 
 class MainActivity : AppCompatActivity() {
     var adapterViewPager: FragmentPagerAdapter? = null
-
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        var all = findViewById<TextView>(R.id.mands) as TextView
+        object : CountDownTimer(65000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                var minutes = (millisUntilFinished / 60000).toString()
+                var seconds = ((millisUntilFinished % 60000) / 1000).toString()
+                all.text= if (seconds.toInt() == 60) minutes + 1 + ":00" else minutes + ":" + (if (seconds.toInt() < 10) "0" else "") + seconds;
+                Log.d("min",(millisUntilFinished / 1000).toString())
+            }
+
+            override fun onFinish() {
+                //mTextField.setText("done!")
+            }
+        }.start()
         val vpPager = findViewById<VerticalViewPager>(R.id.pager) as VerticalViewPager
         adapterViewPager = MyPagerAdapter(supportFragmentManager)
         vpPager.adapter = adapterViewPager
 
+
+        val inLay = findViewById<View>(R.id.innerLay) as LinearLayout
+
+        for (x in 0..9) {
+            inLay.addView(getView(x))
+        }
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+
+        /*val timer = TextView(this)
+        timer.text = "hallo"
+        timer.id = 5
+        timer.setTextAppearance(R.style.fontForNotificationLandingPage);
+        timer.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+
+
+        val timer1 = TextView(this)
+        timer1.text = "ttisdf"
+        timer1.id = 6
+        timer1.setTextAppearance(R.style.fontForNotificationLandingPage);
+        timer1.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+
+        val timer2 = TextView(this)
+        timer2.text = "sdfj"
+        timer2.id = 7
+        timer2.setTextAppearance(R.style.fontForNotificationLandingPage);
+        timer2.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+        (lin as LinearLayout).addView(timer)
+        (lin).addView(timer1)
+        (lin).addView(timer2)*/
+
 
 
         vpPager.addOnPageChangeListener(object : OnPageChangeListener {
@@ -58,6 +116,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun getView(x: Int): View {
+        val rootView = layoutInflater.inflate(R.layout.featured_item, null)
+        return rootView
+    }
+    fun convertSpToPixels(sp: Float, context: Context): Float {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.getResources().getDisplayMetrics())
+    }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
